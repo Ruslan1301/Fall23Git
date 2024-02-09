3fe84559f189cc26ebe3b225e33507b7e270f17f
Ruslan <ruslew13@inbox.ru>
2024-02-09 18:11:31 +0600

    scrum-162/Feature-Visa Payment

---
+++ src/payment/visaPayment.java
@@ -0,0 +1,8 @@
+package payment;
+
+public class visaPayment {
+
+    public void payByVisa(String cardHolderName, int cardNumber){
+        System.out.println(cardHolderName + " " + cardNumber);
+    }
+}
