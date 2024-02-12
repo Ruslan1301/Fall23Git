a596c08ab75d1c9eb604bfab6629798b102f27a5
Ruslan <ruslew13@inbox.ru>
2024-02-12 15:25:58 +0600

    D1-commit-2

--- src/d1/D1.java
+++ src/d1/D1.java
@@ -4,4 +4,8 @@ public class D1 {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+
+    public static void sayHello(){
+        System.out.println("SayHello");
+    }
 }
