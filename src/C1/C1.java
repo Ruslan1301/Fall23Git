897919a4085c4eeedf84c8110075423a8f97704d
Ruslan <ruslew13@inbox.ru>
2024-02-12 15:16:05 +0600

    C1-commit-4

--- src/C1/C1.java
+++ src/C1/C1.java
@@ -1,7 +1,13 @@
 package C1;
 
 public class C1 {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+
+        public static void main (String[]args){
+            System.out.println("Hello");
+        }
+
+        public static void sayHello (){
+            System.out.println("World");
+        }
+
     }
-}
