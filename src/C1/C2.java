4435e2560148e47e22099c5869794624aed2f300
Ruslan <ruslew13@inbox.ru>
2024-02-12 15:24:38 +0600

    D1-commit-1

--- src/C1/C2.java
+++
@@ -1,7 +0,0 @@
-package C1;
-
-public class C2 {
-    public static void main(String[] args) {
-        System.out.println("Hel");
-    }
-}
---
+++ src/d1/D1.java
@@ -0,0 +1,7 @@
+package d1;
+
+public class D1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
