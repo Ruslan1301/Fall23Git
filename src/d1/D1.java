c675670e260af412042abae197344134f45b4e74
Ruslan <ruslew13@inbox.ru>
2024-02-13 14:15:05 +0600

    Revert "Reapply "A1-commit-1-update-new-2"
    This reverts commit af229e8a23ee28c7c2a6ce98398d4449cec48a00.

---
+++ src/d1/D1.java
@@ -0,0 +1,11 @@
+package d1;
+
+public class D1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    public static void sayHello(){
+        System.out.println("SayHello");
+    }
+}
